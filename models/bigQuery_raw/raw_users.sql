f580cdd5ca865fbc03bada0006c92de928c6bbf9
ayaakhaled.data@gmail.com <ayaakhaled.data@gmail.com>
2024-06-06 00:29:13 +0000

    test: Add time difference test in my_model
    - Added a custom dbt test to ensure that the difference between the 'updated_at' field and the current timestamp is less than 2 minutes.
    - This test will help catch discrepancies in data freshness and ensure timely updates.
    - The test is configured to run in production environments only to enforce data freshness SLAs.

---
+++ models/bigQuery_raw/raw_users.sql
@@ -0,0 +1,5 @@
+
+SELECT ID as user_id , 
+       FIRST_NAME as name , 
+      FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', current_timestamp()) as updated_at_utc
+ FROM {{ source('jaffle_shop', 'customers') }}
---
+++ tests/update_test_model.sql
@@ -0,0 +1,21 @@
+-- tests/my_model_test.sql
+
+{% macro time_difference_test() %}
+  {{ config(
+    materialized='view',
+    unique_key='test_id',
+    test_label='Time difference test',
+  ) }}
+
+  SELECT
+    'time_difference_test' AS test_id,
+    CASE
+      WHEN TIMESTAMP_DIFF(FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', current_timestamp()), updated_at_utc, MINUTE) < 2 
+      THEN 'Difference between updated_at and current timestamp is less than 2 minutes'
+      ELSE 'Difference between updated_at and current timestamp is greater than 2 minutes'
+    END AS error_message
+  FROM
+    {{ ref('raw_users') }}
+{% endmacro %}
+
+{{ time_difference_test() }}
